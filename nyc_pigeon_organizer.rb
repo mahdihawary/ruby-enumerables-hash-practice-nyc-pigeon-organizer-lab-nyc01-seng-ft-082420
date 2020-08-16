38c8808faa6441ccd5f77de0bd804d1651017929
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-16 19:52:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  data.each do |key,value|
+    value.each do |new_value,pigeons|
+      pigeons.each do |pigeon|
+        
+        if !new_data[pigeon] 
+          new_data[pigeon] = {}
+        end
+        
+        if !new_data[pigeon][key]
+          new_data[pigeon][key] =[]
+        end
+        
+        new_data[pigeon][key] << new_value.to_s
+      
+      end
+    end
+    
+  end
+  new_data
 end
